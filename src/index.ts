4cf77ff3bed6bea634879d86f8b6742dd5717393
Paweł Romanowski <pawel.romanowski000@gmail.com>
2022-12-07 22:04:36 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,44 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { parser } from "./syntax.grammar";
+import {
+  LRLanguage,
+  LanguageSupport,
+  indentNodeProp,
+  foldNodeProp,
+  foldInside,
+  delimitedIndent,
+} from "@codemirror/language";
+import { completeFromList } from "@codemirror/autocomplete";
+import { styleTags, tags as t } from "@lezer/highlight";
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const FLASHILanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Application: delimitedIndent({ closing: ")", align: false }),
       }),
       foldNodeProp.add({
-        Application: foldInside
+        Application: foldInside,
       }),
       styleTags({
         Identifier: t.variableName,
-        Boolean: t.bool,
         String: t.string,
         LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
+        "( )": t.paren,
+      }),
+    ],
   }),
   languageData: {
-    commentTokens: {line: ";"}
-  }
-})
+    commentTokens: { line: "#" },
+  },
+});
+
+export const FLASHICompletion = FLASHILanguage.data.of({
+  autocomplete: completeFromList([
+    { label: "echo", type: "function" },
+    { label: "press", type: "function" },
+  ]),
+});
 
 export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+  return new LanguageSupport(FLASHILanguage, [FLASHICompletion]);
 }
